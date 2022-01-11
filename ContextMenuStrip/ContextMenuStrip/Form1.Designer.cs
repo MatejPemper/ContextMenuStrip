7c0712192a7b1a3212941198d16fceaf387d7593
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-01-11 10:56:47 +0100

    Add files via upload

---
+++ ContextMenuStrip/ContextMenuStrip/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ContextMenuStrip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtLeft = new System.Windows.Forms.TextBox();
+            this.txtRight = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.leftToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtLeft
+            // 
+            this.txtLeft.ContextMenuStrip = this.contextMenuStrip1;
+            this.txtLeft.Location = new System.Drawing.Point(12, 105);
+            this.txtLeft.Name = "txtLeft";
+            this.txtLeft.Size = new System.Drawing.Size(100, 20);
+            this.txtLeft.TabIndex = 0;
+            // 
+            // txtRight
+            // 
+            this.txtRight.ContextMenuStrip = this.contextMenuStrip1;
+            this.txtRight.Location = new System.Drawing.Point(219, 105);
+            this.txtRight.Name = "txtRight";
+            this.txtRight.Size = new System.Drawing.Size(100, 20);
+            this.txtRight.TabIndex = 1;
+            this.txtRight.TextChanged += new System.EventHandler(this.txtRight_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.leftToolStripMenuItem,
+            this.rightToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // leftToolStripMenuItem
+            // 
+            this.leftToolStripMenuItem.Name = "leftToolStripMenuItem";
+            this.leftToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.leftToolStripMenuItem.Text = "Left";
+            this.leftToolStripMenuItem.Click += new System.EventHandler(this.leftToolStripMenuItem_Click);
+            // 
+            // rightToolStripMenuItem
+            // 
+            this.rightToolStripMenuItem.Name = "rightToolStripMenuItem";
+            this.rightToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rightToolStripMenuItem.Text = "right";
+            this.rightToolStripMenuItem.Click += new System.EventHandler(this.rightToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 185);
+            this.Controls.Add(this.txtRight);
+            this.Controls.Add(this.txtLeft);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLeft;
+        private System.Windows.Forms.TextBox txtRight;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem leftToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rightToolStripMenuItem;
+    }
+}
+
---
+++ ContextMenuStrip/ContextMenuStrip/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContextMenuStrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+        private void leftToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            if(txtLeft.Text == string.Empty)
+            { 
+                MessageBox.Show("PRAZNO!","UPIS", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                txtLeft.Text = txtRight.Text;
+                txtRight.Clear();
+            }
+        }
+
+        private void txtRight_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void rightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            if (txtLeft.Text ==string.Empty)
+            {
+                MessageBox.Show("PRAZNO!","UPISI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                txtRight.Text = txtLeft.Text;
+                txtLeft.Clear();
+            }
+        }
+    }
+}
